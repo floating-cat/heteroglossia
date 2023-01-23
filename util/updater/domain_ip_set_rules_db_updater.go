5952dc6a4045cfdcdd9001f837a1bcad28c3904c
ringo is a color <115804577+ringo-is-a-color@users.noreply.github.com>
2023-01-24 00:32:49 +0800

    Use db file to load rules to reduce the memory usage when running

---
+++ util/updater/domain_ip_set_rules_db_updater.go
@@ -0,0 +1,37 @@
+package updater
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/floating-cat/heteroglossia/conf/rule"
+	"github.com/floating-cat/heteroglossia/util/log"
+)
+
+const (
+	domainIPSetRulesFileURL          = "https://github.com/floating-cat/domain-ip-set-rules/raw/release/domain-ip-set-rules.db"
+	domainIPSetRulesFileSHA256SumURL = "https://github.com/floating-cat/domain-ip-set-rules/raw/release/domain-ip-set-rules.db.sha256sum"
+
+	rulesFileNeedUpdateInterval = 15 * 10 * time.Hour
+)
+
+func UpdateRulesFiles(client *http.Client) (bool, error) {
+	update, err := needUpdateFile(rule.DomainIPSetRulesDBFilePathFromExecutable, rulesFileNeedUpdateInterval)
+	if err != nil {
+		return false, err
+	}
+	if !update {
+		return false, nil
+	}
+
+	err = updateRulesFiles(client)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func updateRulesFiles(client *http.Client) error {
+	log.Info("start to update rules' files")
+	return updateFile(client, rule.DomainIPSetRulesDBFilePathFromExecutable, domainIPSetRulesFileURL, domainIPSetRulesFileSHA256SumURL)
+}
