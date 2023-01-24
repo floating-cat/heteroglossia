d602e2070132b7b5d94e354df2a5e6b8643ab8b3
floating-cat <monsoon.cl@gmail.com>
2023-01-24 13:22:49 +0800

    Clean the logging

--- util/proxy/proxy_fallback.go
+++ util/proxy/proxy_fallback.go
@@ -4,10 +4,9 @@ package proxy
 
 import (
 	"github.com/floating-cat/heteroglossia/transport"
-	"github.com/floating-cat/heteroglossia/util/log"
+	"github.com/floating-cat/heteroglossia/util/errors"
 )
 
 func SetSystemProxy(_ string, _ uint16, _ *transport.HTTPSOCKSAuthInfo) error {
-	log.Info("doesn't support the system proxy in this OS")
-	return nil
+	return errors.New("doesn't support the system proxy in this OS")
 }
