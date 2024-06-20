46de849a96f6d9056f02ee57c6c6c52af974f030
floating-cat <monsoon.cl@gmail.com>
2024-06-20 17:01:31 +0000

    Clean code

--- transport/tu_carrier/client_server_test.go
+++ transport/tu_carrier/client_server_test.go
@@ -5,13 +5,13 @@ import (
 
 	"github.com/floating-cat/heteroglossia/conf"
 	"github.com/floating-cat/heteroglossia/transport"
-	"github.com/floating-cat/heteroglossia/util/test"
+	"github.com/floating-cat/heteroglossia/util/testutil"
 )
 
 func TestClientServerConnection(t *testing.T) {
-	test.TestClientServerConnection(t, newClient, NewServer)
+	testutil.TestClientServerConnection(t, newClient, NewServer)
 }
 
-func newClient(serverConf *conf.Config) (transport.Client, error) {
-	return NewClient(test.ToProxyNode(serverConf.Inbounds.Hg), false)
+func newClient(proxyNode *conf.ProxyNode) (transport.Client, error) {
+	return NewClient(proxyNode, false)
 }
